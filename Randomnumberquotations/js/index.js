2a59840427c47d63ca8ced23c130954ea02e23ac
pushpalatha86 <mailtopushpaa@gmail.com>
2018-05-13 22:04:24 -0500

    Randomnumberquote

---
+++ Randomnumberquotations/js/index.js
@@ -0,0 +1,50 @@
+function clickMe() {
+  var result=Math.floor(Math.random()*10)+1;
+  switch(result)
+    {
+      case 1:
+        document.getElementById("set").style.backgroundImage = "url('http://labs2.kentooz.com/kodok/wp-content/uploads/2013/04/Nature-Wallpaper-99.jpg')"; 
+document.getElementById("backColor").style.backgroundColor="green";     document.getElementById("show").innerHTML = ' " You cannot believe in God until you believe in yourself."<br> -Swami Vivekananda. ';
+        break;
+        case 2:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/blooming_spring-wide.jpg')";  document.getElementById("backColor").style.backgroundColor="YellowGreen";         document.getElementById("show").innerHTML =  ' "Sugar and sand may be mixed together, but the ant rejects the sand and goes off with the sugar grain; so pious men lift the good from the bad." <br> -Ramakrishna</br> ';
+        break;
+        case 3:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/alkaline_lake-wide.jpg')";
+document.getElementById("backColor").style.backgroundColor="yellow";        document.getElementById("show").innerHTML =  ' "End is not the end, if fact E.N.D. means “Effort Never Dies" <br> -A. P. J. Abdul Kalam</br> ';
+        break;
+        case 4:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/moon_clouds_dark_sky-wide.jpg')";
+ document.getElementById("backColor").style.backgroundColor="Purple";         document.getElementById("show").innerHTML =  '"Yesterday is gone. Tomorrow has not yet come. We have only today. Let us begin." <br> -Mother Teresa</br> ';
+        break;
+        case 5:
+ document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/sailing_boat-HD.jpg')";
+        document.getElementById("backColor").style.backgroundColor="GreenYellow";         
+       document.getElementById("show").innerHTML =  ' "Talk to yourself at  least  once in a Day.. Otherwise you may miss a meeting with an EXCELLENT person in this World." <br> -Swami Vivekananda</br> ';
+
+        break;
+        case 6:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/blue_in_abstract-wide.jpg')";
+  document.getElementById("backColor").style.backgroundColor="LightCoral";       document.getElementById("show").innerHTML =  '"Take risks in your life, If you win, you can lead! If you loose, you can guide!"<br> -Swami Vivekananda</br>';
+        break;
+        case 7:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/fall_foliage-wide.jpg')";
+ document.getElementById("backColor").style.backgroundColor="Tomato";         document.getElementById("show").innerHTML =  ' "Arise! Awake! and stop not until the goal is reached." <br> -Swami Vivekananda</br> ';
+        break;
+        case 8:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/hd_beautiful_winter-normal5.4.jpg')";
+  document.getElementById("backColor").style.backgroundColor="SpringGreen";        document.getElementById("show").innerHTML =  '"The future depends on what you do today." <br> – Mahatma Gandhi</br> ';
+        break;
+        case 9:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/foggy_sunrise_4k-wide.jpg')";
+ document.getElementById("backColor").style.backgroundColor="Orchid";  
+        document.getElementById("show").innerHTML =  ' "Three things cannot be long hidden: the sun, the moon, and the truth."<br>   -Buddha</br> ';
+        break;
+        case 10:
+document.getElementById("set").style.backgroundImage = "url('https://www.hdwallpapers.in/walls/sunny_winter_pine_trees_4k-wide.jpg')";
+ document.getElementById("backColor").style.backgroundColor="Orange";  
+        document.getElementById("show").innerHTML = ' "First they ignore you, then they laugh at you, then they fight you, then you win." <br> – Mahatma Gandhi</br> ';
+        break;
+    }
+  
+}
